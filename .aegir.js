422c55232eda03088910d4d1b8a2adecb303f9eb
achingbrain <alex@achingbrain.net>
2021-07-08 15:40:14 +0100

    chore: update deps
    BREAKING CHANGE: uses new peer id, multiaddr, etc

--- .aegir.js
+++ .aegir.js
@@ -5,7 +5,6 @@ const pipe = require('it-pipe')
 const { Multiaddr } = require('multiaddr')
 
 const ma = new Multiaddr('/ip4/127.0.0.1/tcp/12345/http/p2p-webrtc-direct')
-let listener
 
 const mockUpgrader = {
   upgradeInbound: maConn => maConn,
@@ -14,25 +13,28 @@ const mockUpgrader = {
 
 function before () {
   const wd = new WebRTCDirect({ upgrader: mockUpgrader })
-
-  listener = wd.createListener((conn) => pipe(conn, conn))
+  const listener = wd.createListener((conn) => pipe(conn, conn))
   listener.on('listening', () => {
     console.log('listener started on:', ma.toString())
   })
   listener.on('error', console.error)
-  return listener.listen(ma)
+  listener.listen(ma)
+
+  return {
+    listener
+  }
 }
 
-async function after () {
-  await listener.close()
+async function after (testOptions, beforeReturn) {
+  await beforeReturn.listener.close()
   // TODO: Temporary fix per wrtc issue
   // https://github.com/node-webrtc/node-webrtc/issues/636
   process.exit(0)
 }
 
 module.exports = {
-  hooks: {
-    pre: before,
-    post: after
+  test: {
+    before,
+    after
   }
 }
