256e504d6e9a36c91fbd389a040d18883f2fef25
achingbrain <alex@achingbrain.net>
2022-05-04 13:51:13 +0100

    fix: update interfaces
    Update to the latest interfaces version

--- test/listen.ts
+++ test/listen.ts
@@ -1,6 +1,6 @@
 /* eslint-env mocha */
 
-import { expect } from 'aegir/utils/chai.js'
+import { expect } from 'aegir/chai'
 import { Multiaddr } from '@multiformats/multiaddr'
 import { mockRegistrar, mockUpgrader } from '@libp2p/interface-compliance-tests/mocks'
 import { isBrowser } from 'wherearewe'
